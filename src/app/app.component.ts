5c8e7f580978f926cf35c9844f5c75ea56a05cd5
ishikasinghal63-cmyk <ishikasinghal63@gmail.com>
2025-07-19 16:41:05 +0530

    Merged README with local project

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { NgFor, NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,NgIf,NgFor],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  // title = 'myproject';
+  showMore = false;
+   hearts = Array.from({ length: 20 }, () => ({
+    left: Math.floor(Math.random() * 100),
+    delay: Math.random() * 9
+  }));
+
+  today : Date = new Date();
+
+  toggleMessage() {
+    this.showMore = !this.showMore;
+  }
+
+}
