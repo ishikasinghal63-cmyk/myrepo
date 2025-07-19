d864aad233a2392b407e01c66db20dbb98bec11a
ishikasinghal63-cmyk <ishikasinghal63@gmail.com>
2025-07-19 17:41:44 +0530

    Fix working GitHub Pages deployment

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -14,7 +14,7 @@ export default {
   assets: {
     'index.csr.html': {size: 500, hash: '4158af88f132e6f67f8f08a0c3f3bc05af2d24e22d1ccea7879bfd6471bb4291', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
     'index.server.html': {size: 1013, hash: '94d0ada3c6c85ddf3ecbf98f265952779d978625e3cacc4a2426f140874a173e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 6312, hash: '6e818cccc20cdc384d8777c3c682e168a3456d64011c520ced1fd836718a1d27', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.html': {size: 6309, hash: '05360f893ac38d52d9bd82e7dde983971845d7e74fd810a0420e700115ee6680', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
